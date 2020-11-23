3d215e1a81cdbb6788b93bfd5d0f6be1a6234d6a
Geraldine Villacura R <geraldinevillacuraruz@gmail.com>
2020-11-22 23:42:51 -0300

    readme

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 const findLinks = require("./mdLinks");
 const statsLinks = require("./statsLinks");
 
@@ -6,62 +7,56 @@ const option1 = process.argv[3];
 const option2 = process.argv[4];
 
 if (
-  typeof path !== "undefined" &&
-  typeof option1 !== "undefined" &&
-  typeof option1 !== "undefined"
+  (option1 === "--validate" && typeof option2 === "undefined") ||
+  (typeof option1 === "undefined" && option2 === "--validate")
 ) {
-  if (
-    (option1 === "--validate" && typeof option2 === "undefined") ||
-    (typeof option1 === "undefined" && option2 === "--validate")
-  ) {
-    findLinks(path, { validate: true })
-      .then((links) => {
-        console.log(links);
-      })
-      .catch(console.error);
-  } else if (
-    (typeof option1 === "undefined" && option2 === "--stats") ||
-    (option1 === "--stats" && typeof option2 === "undefined")
-  ) {
-    findLinks(path, { validate: true })
-      .then((links) => {
-        statsLinks(links)
-          .then((statsLinks) => {
-            console.log(
-              "Total =",
-              statsLinks.Total,
-              "Unique =",
-              statsLinks.Unique
-            );
-          })
-          .catch(console.error);
-      })
-      .catch(console.error);
-  } else if (
-    (option1 === "--validate" && option2 === "--stats") ||
-    (option1 === "--stats" && option2 === "--validate")
-  ) {
-    findLinks(path, { validate: true })
-      .then((links) => {
-        statsLinks(links)
-          .then((statsLinks) => {
-            console.log(
-              "Total =",
-              statsLinks.Total,
-              "Unique =",
-              statsLinks.Unique,
-              "Broken =",
-              statsLinks.Broken
-            );
-          })
-          .catch(console.error);
-      })
-      .catch(console.error);
-  } else {
-    findLinks(path)
-      .then((links, data) => {
-        console.log(links, data);
-      })
-      .catch(console.error);
-  }
+  findLinks(path, { validate: true })
+    .then((links) => {
+      console.log(links);
+    })
+    .catch(console.error);
+} else if (
+  (typeof option1 === "undefined" && option2 === "--stats") ||
+  (option1 === "--stats" && typeof option2 === "undefined")
+) {
+  findLinks(path, { validate: true })
+    .then((links) => {
+      statsLinks(links)
+        .then((statsLinks) => {
+          console.log(
+            "Total =",
+            statsLinks.Total,
+            "Unique =",
+            statsLinks.Unique
+          );
+        })
+        .catch(console.error);
+    })
+    .catch(console.error);
+} else if (
+  (option1 === "--validate" && option2 === "--stats") ||
+  (option1 === "--stats" && option2 === "--validate")
+) {
+  findLinks(path, { validate: true })
+    .then((links) => {
+      statsLinks(links)
+        .then((statsLinks) => {
+          console.log(
+            "Total =",
+            statsLinks.Total,
+            "Unique =",
+            statsLinks.Unique,
+            "Broken =",
+            statsLinks.Broken
+          );
+        })
+        .catch(console.error);
+    })
+    .catch(console.error);
+} else {
+  findLinks(path)
+    .then((links) => {
+      console.log(links);
+    })
+    .catch(console.error);
 }
